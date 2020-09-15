1803a4c35db410f148e060d7cd02b243c74560f6
C.Thomas <c.tom34270@gmail.com>
2020-09-15 23:12:41 +0200

    update

--- HMIN122M/td/td1/td1.sql
+++ HMIN122M/td/td1/td1.sql
@@ -1,70 +1,70 @@
-DROP TABLE IF EXISTS `mot_clé`
-DROP TABLE IF EXISTS `tag`
-DROP TABLE IF EXISTS `commentaire`
-DROP TABLE IF EXISTS `discussion`
-DROP TABLE IF EXISTS `album`
-DROP TABLE IF EXISTS `galerie`
-DROP TABLE IF EXISTS `configuration`
-DROP TABLE IF EXISTS `appareil_photo`
-DROP TABLE IF EXISTS `utilisateur`
+DROP TABLE IF EXISTS `mot_clé`;
+DROP TABLE IF EXISTS `tag`;
+DROP TABLE IF EXISTS `commentaire`;
+DROP TABLE IF EXISTS `discussion`;
+DROP TABLE IF EXISTS `album`;
+DROP TABLE IF EXISTS `galerie`;
+DROP TABLE IF EXISTS `configuration`;
+DROP TABLE IF EXISTS `appareil_photo`;
+DROP TABLE IF EXISTS `utilisateur`;
 
-CREATE TABLE `utilisateur` {
+CREATE TABLE `utilisateur` (
     idUser NUMBER,
     CONSTRAINT PK_IDUSER PRIMARY KEY (idUser)
-}
+)
 
-CREATE TABLE `photographie` {
+CREATE TABLE `photographie` (
     idPhoto NUMBER,
     licence VARCHAR2(15),
     datePublication DATE,
     nbLike NUMBER,
     CONSTRAINT PK_IDPHOTO PRIMARY KEY (idPhoto),
     CONSTRAINT FK_PHOTO_IDUSER FOREIGN KEY (idUser) REFERENCES utilisateur (idUser)
-}
+)
 
-CREATE TABLE `appareil_photo` {
+CREATE TABLE `appareil_photo` (
     idAppPhoto NUMBER,
     datePrise DATE,
     coordonnee VARCHAR(20),
     CONSTRAINT PK_IDAPPPHOTO PRIMARY KEY (idAppPhoto),
     CONSTRAINT FK_APPPHOTO FOREIGN KEY (idUser) REFERENCES utilisateur (idUser) 
-}
+)
 
-CREATE TABLE `config` {
+CREATE TABLE `config` (
     ouvertureFocal FLOAT(5),
     tempsExpo NUMBER,
     flashActive BOOLEAN,
     distanceFocal FLOAT(5),
-}
+)
 
-CREATE TABLE `mot_clé` {
+CREATE TABLE `mot_clé` (
     mot VARCHAR(20)
-}
+)
 
-CREATE TABLE `tag` {
+CREATE TABLE `tag` (
     mot VARCHAR(20)
-}
+)
 
-CREATE TABLE `album` {
+CREATE TABLE `album` (
     idAlbum INT,
     CONSTRAINT PK_IDALBUM PRIMARY KEY idAlbum,
     CONSTRAINT FK_ALBUM FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto)
-}
+)
 
-CREATE TABLE `galerie` {
+CREATE TABLE `galerie` (
     idGalerie INT,
     CONSTRAINT PK_IDGALERIE PRIMARY KEY idGalerie,
     CONSTRAINT FK_GALERIE FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto) 
-}
+)
 
-CREATE TABLE `discussion` {
+CREATE TABLE `discussion` (
     idDisc INT,
     CONSTRAINT PK_IDDISCUSSION PRIMARY KEY idDisc,
     CONSTRAINT FK_DISCUSSION FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto) 
-}
+)
 
-CREATE TABLE `commentaire` {
+CREATE TABLE `commentaire` (
     idCom INT,
     CONSTRAINT PK_IDCOMMENTAIRE PRIMARY KEY idCom,
     CONSTRAINT FK_COMMENTAIRE FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto)
-}
+)
