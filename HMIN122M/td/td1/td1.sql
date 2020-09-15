4a5542985fb7a928fbbb35b4ddf16e54d78ebcf3
C.Thomas <c.tom34270@gmail.com>
2020-09-15 23:14:07 +0200

    update

--- HMIN122M/td/td1/td1.sql
+++ HMIN122M/td/td1/td1.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS `mot_clé`;
-DROP TABLE IF EXISTS `tag`;
-DROP TABLE IF EXISTS `commentaire`;
-DROP TABLE IF EXISTS `discussion`;
-DROP TABLE IF EXISTS `album`;
-DROP TABLE IF EXISTS `galerie`;
-DROP TABLE IF EXISTS `configuration`;
-DROP TABLE IF EXISTS `appareil_photo`;
-DROP TABLE IF EXISTS `utilisateur`;
+-- DROP TABLE IF EXISTS `mot_clé`;
+-- DROP TABLE IF EXISTS `tag`;
+-- DROP TABLE IF EXISTS `commentaire`;
+-- DROP TABLE IF EXISTS `discussion`;
+-- DROP TABLE IF EXISTS `album`;
+-- DROP TABLE IF EXISTS `galerie`;
+-- DROP TABLE IF EXISTS `configuration`;
+-- DROP TABLE IF EXISTS `appareil_photo`;
+-- DROP TABLE IF EXISTS `utilisateur`;
 
 CREATE TABLE `utilisateur` (
     idUser NUMBER,
