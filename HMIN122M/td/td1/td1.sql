19b2b6a90cea1067243de1ea9ec68c1e480daac9
C.Thomas <c.tom34270@gmail.com>
2020-09-15 23:16:16 +0200

    update

--- HMIN122M/td/td1/td1.sql
+++ HMIN122M/td/td1/td1.sql
@@ -8,12 +8,12 @@
 -- DROP TABLE IF EXISTS `appareil_photo`;
 -- DROP TABLE IF EXISTS `utilisateur`;
 
-CREATE TABLE `utilisateur` (
+CREATE TABLE utilisateur  (
     idUser NUMBER,
     CONSTRAINT PK_IDUSER PRIMARY KEY (idUser)
 );
 
-CREATE TABLE `photographie` (
+CREATE TABLE photographie (
     idPhoto NUMBER,
     licence VARCHAR2(15),
     datePublication DATE,
@@ -22,7 +22,7 @@ CREATE TABLE `photographie` (
     CONSTRAINT FK_PHOTO_IDUSER FOREIGN KEY (idUser) REFERENCES utilisateur (idUser)
 );
 
-CREATE TABLE `appareil_photo` (
+CREATE TABLE appareil_pho (
     idAppPhoto NUMBER,
     datePrise DATE,
     coordonnee VARCHAR(20),
@@ -30,40 +30,40 @@ CREATE TABLE `appareil_photo` (
     CONSTRAINT FK_APPPHOTO FOREIGN KEY (idUser) REFERENCES utilisateur (idUser) 
 );
 
-CREATE TABLE `config` (
+CREATE TABLE conf (
     ouvertureFocal FLOAT(5),
     tempsExpo NUMBER,
     flashActive BOOLEAN,
     distanceFocal FLOAT(5),
 );
 
-CREATE TABLE `mot_clé` (
+CREATE TABLE mot_clé  (
     mot VARCHAR(20)
 );
 
-CREATE TABLE `tag` (
+CREATE TABLE tag  (
     mot VARCHAR(20)
 );
 
-CREATE TABLE `album` (
+CREATE TABLE albu (
     idAlbum INT,
     CONSTRAINT PK_IDALBUM PRIMARY KEY idAlbum,
     CONSTRAINT FK_ALBUM FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto)
 );
 
-CREATE TABLE `galerie` (
+CREATE TABLE galerie  (
     idGalerie INT,
     CONSTRAINT PK_IDGALERIE PRIMARY KEY idGalerie,
     CONSTRAINT FK_GALERIE FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto) 
 );
 
-CREATE TABLE `discussion` (
+CREATE TABLE discussi (
     idDisc INT,
     CONSTRAINT PK_IDDISCUSSION PRIMARY KEY idDisc,
     CONSTRAINT FK_DISCUSSION FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto) 
 );
 
-CREATE TABLE `commentaire` (
+CREATE TABLE commentaire  (
     idCom INT,
     CONSTRAINT PK_IDCOMMENTAIRE PRIMARY KEY idCom,
     CONSTRAINT FK_COMMENTAIRE FOREIGN KEY (idPhoto) REFERENCES photographie (idPhoto)
