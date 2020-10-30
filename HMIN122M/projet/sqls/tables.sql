bd65ed80f862205ba4514b946fa07ce2700e1b9c
C.Thomas <c.tom34270@gmail.com>
2020-10-30 13:49:30 +0100

    update

--- HMIN122M/projet/sqls/tables.sql
+++ HMIN122M/projet/sqls/tables.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS Inscriptions;
-DROP TABLE IF EXISTS Paiements;
-DROP TABLE IF EXISTS Joueurs;
-DROP TABLE IF EXISTS Formats;
-DROP TABLE IF EXISTS Promotions;
-DROP TABLE IF EXISTS Dates;
+DROP TABLE Inscriptions;
+DROP TABLE Paiements;
+DROP TABLE Joueurs;
+DROP TABLE Formats;
+DROP TABLE Promotions;
+DROP TABLE Dates;
 
 CREATE TABLE Paiements (
-    id_joueur INT(7),
-    id_format INT(7),
-    id_promotion INT(7),
-    id_date INT(7),
-    quantite INT(7),
-    type INT(1),
+    id_joueur NUMBER(7),
+    id_format NUMBER(7),
+    id_promotion NUMBER(7),
+    id_date NUMBER(7),
+    quantite NUMBER(7),
+    type NUMBER(1),
     CONSTRAINT FK_PAIEMENT_JOUEUR FOREIGN KEY (id_joueur) REFERENCES Joueurs (id_joueur),
     CONSTRAINT FK_PAIEMENT_FORMAT FOREIGN KEY (id_format) REFERENCES Formats (id_format),
     CONSTRAINT FK_PAIEMENT_PROMOTION FOREIGN KEY (id_promotion) REFERENCES Promotions (id_promotion),
@@ -19,11 +19,11 @@ CREATE TABLE Paiements (
 );
 
 CREATE TABLE Inscriptions (
-    id_joueur INT(7),
-    id_format INT(7),
-    id_promotion INT(7),
-    id_date INT(7),
-    prix_inscription INT(7),
+    id_joueur NUMBER(7),
+    id_format NUMBER(7),
+    id_promotion NUMBER(7),
+    id_date NUMBER(7),
+    prix_inscription NUMBER(7),
     CONSTRAINT FK_PAIEMENT_JOUEUR FOREIGN KEY (id_joueur) REFERENCES Joueurs (id_joueur),
     CONSTRAINT FK_PAIEMENT_FORMAT FOREIGN KEY (id_format) REFERENCES Formats (id_format),
     CONSTRAINT FK_PAIEMENT_PROMOTION FOREIGN KEY (id_promotion) REFERENCES Promotions (id_promotion),
@@ -31,12 +31,12 @@ CREATE TABLE Inscriptions (
 );
 
 CREATE TABLE Joueurs (
-    id_joueur INT(7),
+    id_joueur NUMBER(7),
     nom VARCHAR(40),
     prenom VARCHAR(40),
     pseudo VARCHAR(40),
-    solde INT(8),
-    type INT(1),
+    solde NUMBER(8),
+    type NUMBER(1),
     email VARCHAR(255),
     date_naissance Date,
     adresse VARCHAR(255),
@@ -48,32 +48,32 @@ CREATE TABLE Joueurs (
 );
 
 CREATE TABLE Formats (
-    id_format INT(7),
+    id_format NUMBER(7),
     type VARCHAR(127),
     taxe DECIMAL(3),
-    montant_min INT(7),
-    montant_max INT(7),
+    montant_min NUMBER(7),
+    montant_max NUMBER(7),
     forme_monetaire VARCHAR(127),
     CONSTRAINT PK_ID_FORMAT PRIMARY KEY (id_format)
 );
 
 CREATE TABLE Promotions (
-    id_promotion INT(7),
+    id_promotion NUMBER(7),
     nom VARCHAR(255),
     reduction DECIMAL(3),
     type VARCHAR(127),
-    date_debut INT(7),
-    date_fin INT(7),
+    date_debut NUMBER(7),
+    date_fin NUMBER(7),
     CONSTRAINT PK_ID_PROMOTION PRIMARY KEY (id_promotion),
     CONSTRAINT FK_PROMOTION_DATE_DEBUT FOREIGN KEY (date_debut) REFERENCES Dates (id_date),
     CONSTRAINT FK_PROMOTION_DATE_FIN FOREIGN KEY (date_fin) REFERENCES Dates (id_date)
 );
 
 CREATE TABLE Dates (
-    id_date INT(7),
+    id_date NUMBER(7),
     timestamp Date,
-    mois INT(2),
-    annee INT(4),
-    fuseau INT(2),
+    mois NUMBER(2),
+    annee NUMBER(4),
+    fuseau NUMBER(2),
     CONSTRAINT PK_ID_DATE PRIMARY KEY (id_date)
 );
