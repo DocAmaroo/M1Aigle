ead6bcd26cbbab0e93c6dc68cf4be40a3dd50bb7
C.Thomas <c.tom34270@gmail.com>
2020-11-06 08:53:15 +0100

    update

--- HMIN103/MEAN/serveur.js
+++ HMIN103/MEAN/serveur.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app     = express();
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -10,69 +12,82 @@ app.use(function (req, res, next) {
 });
 
 const MongoClient = require('mongodb').MongoClient;
-const ObjectID    = require('mongodb').ObjectId;
-const url         = "mongodb://localhost:27017";
+const ObjectID = require('mongodb').ObjectId;
+const url = "mongodb://localhost:27017";
 
-MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
+MongoClient.connect(url, {
+    useNewUrlParser: true
+}, (err, client) => {
     let db = client.db("SUPERVENTES");
-    
+
     /* Liste des produits */
-    app.get("/produits", (req,res) => {
+    app.get("/produits", (req, res) => {
         console.log("/produits");
         try {
             db.collection("produits").find().toArray((err, documents) => {
                 res.end(JSON.stringify(documents));
             });
-        } catch(e) {
+        } catch (e) {
             console.log("Erreur sur /produits : " + e);
             res.end(JSON.stringify([]));
         }
     });
 
     /* Liste des produits suivant une catégorie */
-    app.get("/produits/:categorie", (req,res) => {
-	let categorie = req.params.categorie;
-        console.log("/produits/"+categorie);
+    app.get("/produits/:categorie", (req, res) => {
+        let categorie = req.params.categorie;
+        console.log("/produits/" + categorie);
         try {
-            db.collection("produits").find({type:categorie}).toArray((err, documents) => {
+            db.collection("produits").find({
+                type: categorie
+            }).toArray((err, documents) => {
                 res.end(JSON.stringify(documents));
             });
-        } catch(e) {
-            console.log("Erreur sur /produits/"+categorie+" : "+ e);
+        } catch (e) {
+            console.log("Erreur sur /produits/" + categorie + " : " + e);
             res.end(JSON.stringify([]));
         }
     });
 
-    
+
     /* Liste des catégories de produits */
-    app.get("/categories", (req,res) => {
+    app.get("/categories", (req, res) => {
         console.log("/categories");
-	    categories = [];
+        categories = [];
         try {
             db.collection("produits").find().toArray((err, documents) => {
-		    for (let doc of documents) {
-                if (!categories.includes(doc.type)) categories.push(doc.type); 
-		    }
-            res.end(JSON.stringify(categories));
+                for (let doc of documents) {
+                    if (!categories.includes(doc.type)) categories.push(doc.type);
+                }
+                res.end(JSON.stringify(categories));
             });
-        } catch(e) {
+        } catch (e) {
             console.log("Erreur sur /categories : " + e);
             res.end(JSON.stringify([]));
         }
     });
 
     /* Connexion */
-    app.post("/membre/connexion", (req,res) => {
+    app.post("/membre/connexion", (req, res) => {
         try {
             db.collection("membres")
-            .find(req.body)
-            .toArray((err, documents) => {
-                if (documents != undefined && documents.length == 1)
-                    res.end(JSON.stringify({"resultat": 1, "message": "Authentification réussie"}));
-                else res.end(JSON.stringify({"resultat": 0, "message": "Email et/ou mot de passe incorrect"}));
-            });
+                .find(req.body)
+                .toArray((err, documents) => {
+                    if (documents != undefined && documents.length == 1)
+                        res.end(JSON.stringify({
+                            "resultat": 1,
+                            "message": "Authentification réussie"
+                        }));
+                    else res.end(JSON.stringify({
+                        "resultat": 0,
+                        "message": "Email et/ou mot de passe incorrect"
+                    }));
+                });
         } catch (e) {
-            res.end(JSON.stringify({"resultat": 0, "message": e}));
+            res.end(JSON.stringify({
+                "resultat": 0,
+                "message": e
+            }));
         }
     });
 });
